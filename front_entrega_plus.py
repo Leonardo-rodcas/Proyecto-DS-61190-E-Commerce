371c3fb31784cbabab8e3d13b9f2529172b9b407
Leonardo-rodcas <106358248+Leonardo-rodcas@users.noreply.github.com>
2024-12-28 02:47:45 -0300

    nuevo

--- front_entrega_plus.py
+++ front_entrega_plus.py
@@ -2,44 +2,49 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("dataset/traj_UNI_CORR_500_01.txt", 
-                 skiprows=4, 
-                 sep="\t", 
-                 names=["ID", "frames", "X", "Y", "Z"])
-    
-st.write("""
+data = {
+    'Mes':['Enero', 'Febrero'],
+    'Ventas': [250,300]
+}
+
+df = pd.DataFrame(data)
+print(df)
+df.plot(kind='line', x='Mes', y='Ventas', marker='o', title='Ventas Mensuales')
+plt.ylabel('Ventas en Pesos')
+plt.xlabel('Mes')
+plt.grid(True)  
+plt.show()  
+
+#st.write("""
 # Mi primera aplicación interactiva
 ## Histograma sobre el eje X e Y
-""")
+#""")
 
 # Using "with" notation
-with st.sidebar:
+#with st.sidebar:
     # Titulo
-    st.write("# Opciones")
+#    st.write("# Opciones")
     # slider
-    div = st.slider('Número de bins:', 0, 130, 25)
-    st.write("Bins=", div)
+ #   div = st.slider('Número de bins:', 0, 130, 25)
+  #  st.write("Bins=", div)
 
 # Desplegamos un histograma con los datos del eje X
-fig, ax = plt.subplots(1, 2, figsize=(10, 3))
-ax[0].hist(df["X"], bins=div, color= "lightcoral")
-ax[0].set_xlabel('Posición en metros')
-ax[0].set_ylabel('Frecuencia')
-ax[0].set_title('Histograma posiciónes en el eje X')
+#fig, ax = plt.subplots(1, 2, figsize=(10, 3))
+#ax[0].hist(df["X"], bins=div, color= "lightcoral")
+#ax[0].set_xlabel('Posición en metros')
+#ax[0].set_ylabel('Frecuencia')
+#ax[0].set_title('Histograma posiciónes en el eje X')
 
-ax[1].hist(df["Y"], bins=div, color= "indianred")
-ax[1].set_xlabel('Posición en metros')
-ax[1].set_ylabel('Frecuencia')
-ax[1].set_title('Histograma posiciónes en el eje Y')
+#ax[1].hist(df["Y"], bins=div, color= "indianred")
+#ax[1].set_xlabel('Posición en metros')
+#ax[1].set_ylabel('Frecuencia')
+#ax[1].set_title('Histograma posiciónes en el eje Y')
 
 # Desplegamos el gráfico
-st.pyplot(fig)
+#st.pyplot(fig)
 
-st.write("""
+#st.write("""
 ## Muestra de datos cargados
-""")
+#""")
 # Graficamos una tabla
-st.table(df.head())
-
-
-
+#st.table(df.head())
